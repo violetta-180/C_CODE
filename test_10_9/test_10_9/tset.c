0db0df5f4f9837df0845b723d6375a0f523f52fd
violetta <Jzh200410.>
2025-10-09 23:15:49 +0800

    10-9game1

---
+++ test_10_9/test_10_9/tset.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+//二维数组的数组名的理解
+//int main()
+//{
+//	int arr[3][4] = {0};
+//	printf("%d\n", sizeof(arr) / sizeof(arr[0]));//行	
+//	printf("%d\n", sizeof(arr[0]) / sizeof(arr[0][0]));//列
+//	//printf("%p\n",arr);二维数组的数组名也表示首元素的地址
+//	//printf("%p\n", arr+1);
+//	return 0;
+//}
